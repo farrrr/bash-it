c1592e64d1014e036de365e1039af30d08dad1c1
Far Tseng <farrrr@gmail.com>
2016-12-27 14:53:59 +0800

    remove GREP_OPTIONS
    Signed-off-by: Far Tseng <farrrr@gmail.com>

--- custom/themes/far/far.theme.bash
+++ custom/themes/far/far.theme.bash
@@ -91,9 +91,6 @@ export LC_TIME=en_US.UTF-8
 # Don’t clear the screen after quitting a manual page
 export MANPAGER="less -X"
 
-# Always enable colored `grep` output
-export GREP_OPTIONS="--color=auto"
-
 export LS_COLORS='no=0:fi=0:di=32:ln=36:or=1;40:mi=1;40:pi=31:so=33:bd=44;37:cd=44;37:ex=35:*.jpg=1;32:*.jpeg=1;32:*.JPG=1;32:*.gif=1;32:*.png=1;32:*.jpeg=1;32:*.ppm=1;32:*.pgm=1;32:*.pbm=1;32:*.c=1;33:*.C=1;33:*.h=1;33:*.cc=1;33:*.awk=1;33:*.pl=1;33:*.gz=1;31:*.tar=1;31:*.zip=1;31:*.lha=1;31:*.lzh=1;31:*.arj=1;31:*.tgz=1;31:*.taz=1;31:*.html=1;34:*.htm=1;34:*.doc=1;34:*.txt=1;34:*.o=1;36:*.a=1;36'
 
 export LSCOLORS='gxfxbEaEBxxEhEhBaDaCaD'

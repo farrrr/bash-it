03bf0199edfbb195da359e0551545d38fbe1822f
Far Tseng <farrrr@gmail.com>
2018-06-12 18:22:10 +0800

    update.

---
+++ plugins/available/android.plugin.bash
@@ -0,0 +1,6 @@
+cite about-plugin
+about-plugin 'Android HOME environment'
+
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/platform-tools
